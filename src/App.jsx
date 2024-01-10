c5c3f9f830e803d4771e7e686d97c80f49771207
Sabrina <126303280+sabbycodes@users.noreply.github.com>
2024-01-10 16:54:35 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Canvas from "./canvas"
+import Customizer from "./pages/customize"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <main className="app trabsition-allease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/AIPicker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
+const AIPicker = () => {
+  const snap = useSnapshot(state)
+  return (
+    <div className='absolute left-full ml-3'>
+      l
+    </div>
+  )
+}
+
+export default AIPicker
